84c27fc3bd989cd677ecaabb6de7645c5830d32b
ThomasHascat <dutourne.t@gmail.com>
2023-07-11 12:05:54 +0200

    :add dynamyque filter

--- FrontEnd/assets/js/Filters.js
+++ FrontEnd/assets/js/Filters.js
@@ -9,17 +9,37 @@ async function displayWorks() {
   //FETCH API WORKS
   const response = await fetch("http://localhost:5678/api/works");
   const works = await response.json();
+  const galleryContainer = document.querySelector(".gallery");
 
 
   console.log(works);
   //PARCOUR LE TABLEAU POUR RENVOYER TOUS LES ELEMENTS
   for (let index = 0; index < works.length; index++) {
+    const work = works[index];
 
-    console.log(works[index].title);
+    const figure = document.createElement("figure");
+    const image = document.createElement("img");
 
+    image.src = work.imageUrl;
+    image.alt = work.title;
+    figure.appendChild(image);
+
+
+    const figcaption = document.createElement("figcaption");
+    figcaption.textContent = work.title;
+    figure.appendChild(figcaption);
+
+    galleryContainer.appendChild(figure);
+    console.log(works[index]);
   }
-
+  galleryContainer.appendChild(figure);
 }
+
+
+
+
+
+
 
 
 
@@ -34,10 +54,36 @@ async function filterWorksByCatObjets(categoryId) {
 
 
   console.log(result);
+  const galleryContainer = document.querySelector(".gallery");
+  for (let index = 0; index < result.length; index++) {
+    const work = result[index];
+
+    const figure = document.createElement("figure");
+    const image = document.createElement("img");
+
+    image.src = work.imageUrl;
+    image.alt = work.title;
+    figure.appendChild(image);
+
+
+    const figcaption = document.createElement("figcaption");
+    figcaption.textContent = work.title;
+    figure.appendChild(figcaption);
+
+
+    console.log(works[index]);
 
 
 
+    galleryContainer.appendChild(figure);
+
+
+  }
 }
+
+
+
+
 
 var Appartements = document.querySelector(".Filter-Appartements");
 Appartements.addEventListener('click', () => filterWorksByCatAppartements(2));
@@ -49,6 +95,31 @@ async function filterWorksByCatAppartements(categoryId) {
 
 
   console.log(result);
+  const galleryContainer = document.querySelector(".gallery");
+  for (let index = 0; index < result.length; index++) {
+    const work = result[index];
+
+    const figure = document.createElement("figure");
+    const image = document.createElement("img");
+
+    image.src = work.imageUrl;
+    image.alt = work.title;
+    figure.appendChild(image);
+
+
+    const figcaption = document.createElement("figcaption");
+    figcaption.textContent = work.title;
+    figure.appendChild(figcaption);
+
+
+    
+
+
+
+    galleryContainer.appendChild(figure);
+
+
+  }
 
 
 
@@ -65,6 +136,31 @@ async function filterWorksByCatHotelsRestaurants(categoryId) {
 
 
   console.log(result);
+  const galleryContainer = document.querySelector(".gallery");
+  for (let index = 0; index < result.length; index++) {
+    const work = result[index];
+
+    const figure = document.createElement("figure");
+    const image = document.createElement("img");
+
+    image.src = work.imageUrl;
+    image.alt = work.title;
+    figure.appendChild(image);
+
+
+    const figcaption = document.createElement("figcaption");
+    figcaption.textContent = work.title;
+    figure.appendChild(figcaption);
+
+
+    
+
+
+
+    galleryContainer.appendChild(figure);
+
+
+  }
 
 
 
