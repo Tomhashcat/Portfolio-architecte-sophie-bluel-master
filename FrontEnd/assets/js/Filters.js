53d96b85d5686071b84dab2ca1fe85e1049dbbdc
ThomasHascat <dutourne.t@gmail.com>
2023-07-11 11:04:22 +0200

    :add function displayWorks au click

--- FrontEnd/assets/js/Filters.js
+++ FrontEnd/assets/js/Filters.js
@@ -1,3 +1,42 @@
-function filterTravaux(categoryId){
+
+var Tous = document.querySelector(".Filter-All");
+Tous.addEventListener('click', displayWorks);
+/**
+ * DISPLAYWORKS : Au click renvoi tous les works
+ * @param {func} 
+ */
+async function displayWorks() {
+  //FETCH API WORKS
+  const response = await fetch("http://localhost:5678/api/works");
+  const works = await response.json();
+
+
+  console.log(works);
+  //PARCOUR LE TABLEAU POUR RENVOYER TOUS LES ELEMENTS
+  for (let index = 0; index < works.length; index++) {
+
+    console.log(works[index].title);
+
+  }
 
 }
+displayWorks();
+
+
+
+var Objets = document.querySelector(".Filter-Objets");
+Objets.addEventListener('click',filterWorksByCatObjets);
+
+async function filterWorksByCatObjets(categories) {
+  const response = await fetch("http://localhost:5678/api/works");
+  const works = await response.json();
+  const result = works.filter(work => work.categoryId == categories);
+
+
+  console.log(result);
+
+
+
+}
+
+filterWorksByCatObjets(1);
