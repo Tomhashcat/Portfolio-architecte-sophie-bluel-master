1d0a7b45b9a6c4eca428ed13bccb802993c026b3
ThomasHascat <dutourne.t@gmail.com>
2023-07-13 15:29:29 +0200

    :add renvoie vers index

--- FrontEnd/assets/js/login.js
+++ FrontEnd/assets/js/login.js
@@ -10,39 +10,47 @@ async function handleLogin(event) {
   var email = emailInput.value;
   var password = passwordInput.value;
 
-  const response = await fetch("http://localhost:5678/api/users/login",{
-    method:'POST',
-    headers:{
-      'Accept':'application/json',
-      'Content-Type':'apllication/json'
+  const response = await fetch("http://localhost:5678/api/users/login", {
+    method: 'POST',
+    headers: {
+      'authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY1MTg3NDkzOSwiZXhwIjoxNjUxOTYxMzM5fQ.JGN1p8YIfR-M-5eQ-Ypy6Ima5cKA4VbfL2xMr2MgHm4',
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
     },
-    body:JSON.stringify({
-      email: email,
-      password: password
+    body: JSON.stringify({
+     'email':'sophie.bluel@test.tld' ,
+      'password':'$2b$10$OyUbvsLMHp/gu0kPRpkJ/.757z.DBecw/wd1CE09Y3q4nDjoC7L1e'
     })
   });
 
   if (response.ok) {
-    const htmlResponse = await response.text();
-    displayHTMLResponse(htmlResponse);
+    const jsonResponse = await response.json();
+    console.log(jsonResponse);
 
-    let userExists = false;
-    let userId = null;
-   
-    for (let i = 0; i < users.length; i++) {
-      if (users[i].email === email && users[i].password === password) {
-        userExists = true;
-        userId = users[i].userId;
-        break;
+    const usersResponse = await fetch("http://localhost:5678/api/users");  
+    if (usersResponse.ok) {
+      const users = await usersResponse.json();  
+      let userExists = false;
+      let userId = null;
+
+      for (let i = 0; i < users.length; i++) {
+        if (users[i].email === email && users[i].password === password) {
+          userExists = true;
+          userId = users[i].userId;
+        
+          break;
+        }
       }
-    }
 
-    if (userExists) {
-      console.log('Utilisateur trouvé');
-      console.log('userId:', userId);
-     
+      if (userExists) {
+        console.log('Utilisateur trouvé');
+        console.log('userId:', userId);
+        window.location.href = 'index.html'; 
+      } else {
+        console.log('Utilisateur non trouvé');
+      }
     } else {
-      console.log('Utilisateur non trouvé');
+      console.log('Erreur lors de la récupération des utilisateurs depuis l\'API');
     }
   } else {
     console.log('Erreur lors de la récupération du contenu HTML');
@@ -51,6 +59,6 @@ async function handleLogin(event) {
 
 function displayHTMLResponse(htmlResponse) {
   var loginForm = document.querySelector(".loginForm");
-  loginForm.innerHTML= htmlResponse;
+  loginForm.innerHTML = htmlResponse;
   console.log(htmlResponse);
 }
