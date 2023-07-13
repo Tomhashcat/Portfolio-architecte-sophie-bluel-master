87c10e33e5e19276c1a993988f73dc7b56c7fbe5
ThomasHascat <dutourne.t@gmail.com>
2023-07-13 11:18:43 +0200

    :add POST bug connection

--- FrontEnd/assets/js/login.js
+++ FrontEnd/assets/js/login.js
@@ -1,25 +1,56 @@
-var buttonLog = document.querySelector('.connect');
-var Email=document.querySelector('.mail');
-var password=document.querySelector('.pass');
+var loginForm = document.querySelector(".loginForm");
+loginForm.addEventListener('submit', handleLogin);
 
-buttonLog.addEventListener('click', LogIn)
+async function handleLogin(event) {
+  event.preventDefault();
 
-async function LogIn(){
-const response = await fetch("http://localhost:5678/api/users/login");
-const users =await  response.json();
+  var emailInput = document.querySelector('.email');
+  var passwordInput = document.querySelector('.password');
 
-let userExists =false;
+  var email = emailInput.value;
+  var password = passwordInput.value;
 
-for (let i = 0; i < users.length; i++) {
- if (users[i].email===Email.value){
-  userExists=true;
- 
- }
-  
+  const response = await fetch("http://localhost:5678/api/users/login",{
+    method:'POST',
+    headers:{
+      'Accept':'application/json',
+      'Content-Type':'apllication/json'
+    },
+    body:JSON.stringify({
+      email: email,
+      password: password
+    })
+  });
+
+  if (response.ok) {
+    const htmlResponse = await response.text();
+    displayHTMLResponse(htmlResponse);
+
+    let userExists = false;
+    let userId = null;
+   
+    for (let i = 0; i < users.length; i++) {
+      if (users[i].email === email && users[i].password === password) {
+        userExists = true;
+        userId = users[i].userId;
+        break;
+      }
+    }
+
+    if (userExists) {
+      console.log('Utilisateur trouvé');
+      console.log('userId:', userId);
+     
+    } else {
+      console.log('Utilisateur non trouvé');
+    }
+  } else {
+    console.log('Erreur lors de la récupération du contenu HTML');
+  }
 }
-if(userExists){
-console.log('user trouvé')
-}else{
-  console.log('user non-trouvé')
-}
+
+function displayHTMLResponse(htmlResponse) {
+  var loginForm = document.querySelector(".loginForm");
+  loginForm.innerHTML= htmlResponse;
+  console.log(htmlResponse);
 }
