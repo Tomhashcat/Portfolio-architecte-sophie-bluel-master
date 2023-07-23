2915d5582484150a9529022334bb0b625f0ae5f6
ThomasHascat <dutourne.t@gmail.com>
2023-07-23 18:14:35 +0200

    :add toggle + secondeModal

--- FrontEnd/assets/js/modals.js
+++ FrontEnd/assets/js/modals.js
@@ -1,10 +1,116 @@
 
+let isFirstModalOpen = false;
 
-function showModal() {
+function toggleModal() {
+    var modal = document.getElementById('mod');
+    var secondeModal = document.getElementById('seconde-modale');
+    if (modal.style.display === 'none') {
+        modal.style.display = 'flex';
 
-    // Appele la fonction de filtrage initiale pour afficher toutes les œuvres
+        if (isFirstModalOpen) {
+            
+            generateSecondModal();
+        }
+    } else {
+        modal.style.display = 'none';
+        secondeModal.style.display='flex';
+    }
+    isFirstModalOpen = !isFirstModalOpen;
+}
+
+function handleWorkClick(figureElement) {
+    const workIdToDelete = figureElement.dataset.workId;
+    fetch(`http://localhost:5678/api/works/${workIdToDelete}`, {
+        method: 'DELETE',
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log('oeuvre supprimée avec succes', data);
+            generateFirstModal();
+        })
+        .catch(error => {
+            console.error('Une erreur s\'est produite lors de la suppression de l\'œuvre:', error);
+        });
+}
+
+
+function generateFirstModal() {
+
     const btnsContainer = document.querySelector('.btns-Modif');
     const modGalleryContainer = document.querySelector('.mod-gallery');
+
+
+    const btnModal = document.createElement('input');
+
+    modGalleryContainer.innerHTML = "";
+
+    btnModal.type = 'submit';
+    btnModal.className = 'btn-add-photo';
+    btnModal.textContent = 'Ajouter une photo';
+    const existingBtnAddPhoto = btnsContainer.querySelector('.btn-add-photo');
+    if (!existingBtnAddPhoto) {
+        btnsContainer.appendChild(btnModal);
+    }
+    btnModal.addEventListener('click', () => {
+        toggleModal(); 
+    });
+
+
+
+    const deleteWorkBtn = document.createElement('a');
+    deleteWorkBtn.className = ('btn-delete-work');
+
+
+
+    // Récupére tous les works
+    fetch('http://localhost:5678/api/works')
+        .then(response => response.json())
+        .then(works => {
+
+
+            works.forEach(work => {
+                const figure = document.createElement('figure');
+                const image = document.createElement('img');
+                image.src = work.imageUrl;
+                image.alt = work.title;
+                figure.appendChild(image);
+
+                const figcaption = document.createElement('figcaption');
+                figcaption.textContent = work.title;
+                figure.appendChild(figcaption);
+                figure.setAttribute('data-workid', work._id);
+
+                // Ajouter l'événement de clic pour la suppression des œuvres
+                figure.addEventListener('click', () => handleWorkClick(figure));
+
+                modGalleryContainer.appendChild(figure);
+            });
+        })
+        .catch(error => {
+            console.log('Une erreur s\'est produite lors de la récupération des works :', error);
+        });
+
+    const figureElement = modGalleryContainer.querySelectorAll('figure');
+
+    figureElement.forEach(figure => {
+        if (!figure.dataset.workid) {
+            const workId = '1';
+            figure.setAttribute('data-workid', workId);
+        }
+        if (!figure.hasAttribute('data-click-event-added')) {
+            figure.addEventListener('click', () => handleWorkClick(figure));
+            figure.setAttribute('data-click-event-added', true);
+        }
+    });
+
+
+    deleteWorkBtn.addEventListener('click', () => {
+        const workIdToDelete = deleteWorkBtn.dataset.workid;
+        deleteWork(workIdToDelete);
+    });
+
+
+
 
     if (!btnsContainer.querySelector('.btn-add-photo')) {
 
@@ -14,10 +120,10 @@ function showModal() {
         inputSubmit.value = 'Envoyer une photo';
 
         btnsContainer.appendChild(inputSubmit);
-      
-        btnModal.addEventListener('click', () => {
+
+        inputSubmit.addEventListener('click', () => {
             // Ouvrir la nouvelle page modal générée par la fonction
-            OpenAddWorkModal();
+            generateSecondModal();
         });
 
 
@@ -29,7 +135,10 @@ function showModal() {
         deleteLInk.href = '#';
         deleteLInk.textContent = 'supprimer la galerie';
         deleteLInk.className = 'btn-delete-work';
-        btnsContainer.appendChild(deleteLInk)
+        btnsContainer.appendChild(deleteLInk);
+        deleteLInk.addEventListener('click', () => {
+
+        })
     }
 
 
@@ -38,41 +147,20 @@ function showModal() {
 
 
 
-    // Récupére tous les works
-    fetch('http://localhost:5678/api/works')
-        .then(response => response.json())
-        .then(works => {
 
-            modGalleryContainer.innerHTML = '';
-
-            // Affiche chaque work dans modGalleryContainer
-            works.forEach(work => {
-                const figure = document.createElement('figure');
-                const image = document.createElement('img');
-                image.src = work.imageUrl;
-                image.alt = work.title;
-                figure.appendChild(image);
-
-                const figcaption = document.createElement('figcaption');
-                figcaption.textContent = work.title;
-                figure.appendChild(figcaption);
-
-                modGalleryContainer.appendChild(figure);
-            });
-        })
-        .catch(error => {
-            console.log('Une erreur s\'est produite lors de la récupération des works :', error);
-        });
 }
 
 
 document.addEventListener('DOMContentLoaded', () => {
+    generateSecondModal();
+    const btnModal = document.querySelector('.btn-modal');
+    const btnAddPhoto = document.querySelector('.btn-add-photo');
+    btnModal.addEventListener('click', () => generateFirstModal());
+    btnAddPhoto.addEventListener('click', () => toggleModal());
+});
 
+function generateSecondModal() {
 
-    btnModal.addEventListener('click', () => showModal());
-})
-
-function OpenAddWorkModal() {
     // Créez les éléments pour la nouvelle modal et les éléments nécessaires
     const uploadPhotoContainer = document.createElement('div');
     uploadPhotoContainer.className = "upload-photo-container";
@@ -121,7 +209,27 @@ function OpenAddWorkModal() {
             // ... (votre code pour ajouter le nouveau travail)
         }
     });
+    btnModal.addEventListener('click', () => generateSecondModal);
 }
+
+
+function deleteWork(workId) {
+    fetch(`http://localhost:5678/api/works/${workId}`, {
+        method: 'DELETE',
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Œuvre supprimée avec succès:', data);
+            generateFirstModal();
+        })
+        .catch(error => {
+            console.error('Une erreur s\'est produite lors de la suppression de l\'œuvre:', error);
+        });
+
+}
+
+
+
 
 
 
