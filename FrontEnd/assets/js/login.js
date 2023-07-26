54106d376b750e26a36d13c22193439aabb83564
ThomasHascat <dutourne.t@gmail.com>
2023-07-26 12:41:55 +0200

    :built alerte 404 error login

--- FrontEnd/assets/js/login.js
+++ FrontEnd/assets/js/login.js
@@ -8,6 +8,9 @@ async function handleLogin(event) {
 
   var emailInput = document.querySelector('.email');
   var passwordInput = document.querySelector('.password');
+  var messageError = document.querySelector('.error-message');
+  var divError = document.querySelector('.error');
+  var errorClose = document.querySelector('.error-close');
 
   var email = emailInput.value;
   var password = passwordInput.value;
@@ -38,12 +41,28 @@ async function handleLogin(event) {
       localStorage.setItem('token', token);
       window.location.href = 'index.html';
 
-   
+
     } else {
       console.log('Utilisateur non trouvé');
+
+
+/**
+ * GESTION DES ERREURS
+ */
+      messageError.textContent = "Erreur 404 ! Utilisateur ou mot de passe invalide";
+      divError.style.display = 'flex';
+      errorClose.addEventListener('click', function () {
+        window.location.href = 'login.html';
+      })
+
     }
   } else {
     console.log('Erreur lors de la récupération du contenu HTML');
+    messageError.textContent = "Erreur 404 ! Utilisateur ou mot de passe invalide";
+    divError.style.display = 'flex';
+    errorClose.addEventListener('click', function () {
+      window.location.href = 'login.html';
+    })
     // Affiche la réponse complète de l'API pour déboguer
     const responseText = await response.text();
     console.log('Réponse de l\'API:', responseText);
