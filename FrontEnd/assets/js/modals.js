1d695503b6f4b184b2b48a375b982b371439eba7
ThomasHascat <dutourne.t@gmail.com>
2023-07-30 13:06:03 +0200

    :ajust declaration des variables

--- FrontEnd/assets/js/modals.js
+++ FrontEnd/assets/js/modals.js
@@ -2,11 +2,40 @@
 let isFirstModalOpen = false;
 var mod = document.getElementById('mod');
 var modal = document.querySelector('.modal');
-
+var modalClose;
 var firstModalDiv;
 var secondModalDiv;
+var secondeModalTitre;
 var boutonEnvoyer;
 let selectedWorkIds = [];
+var griseBar;
+var modalTitre;
+var btnValider;
+var btnsContainer;
+var modGalleryContainer;
+var deleteWorkBtn;
+var workId;
+var trashIcon;
+var figcaption;
+var figureElement;
+var deleteLInk;
+var btnOpenFirstModal;
+var uploadPhotoContainer;
+var file;
+var inputPhoto;
+var inputChoiceContent;
+var labelPhoto;
+var labelTitre;
+var inputTitle;
+var labelCategorie;
+var selectCategories;
+var labelCategories;
+var btnModal;
+var imagePreview;
+var btnsContainer;
+var option;
+var existingImagePreview;
+var modalCloseLink;
 
 function toggleModal() {
 
@@ -47,7 +76,7 @@ function generateFirstModal() {
     firstModalDiv.className = "First-modal";
     modal.appendChild(firstModalDiv);
 
-    const modalClose = document.createElement('a');
+     modalClose = document.createElement('a');
     modalClose.className = "modal_close";
     modalClose.textContent = "x";
     firstModalDiv.appendChild(modalClose);
@@ -61,20 +90,20 @@ function generateFirstModal() {
         window.location.href = "index.html";
     });
 
-    const griseBar = document.createElement('div');
+     griseBar = document.createElement('div');
     griseBar.className = 'grise-bar';
     firstModalDiv.appendChild(griseBar);
 
-    const modalTitre = document.createElement('h3');
+     modalTitre = document.createElement('h3');
     modalTitre.className = 'modal_Tiltle';
     modalTitre.textContent = 'Gallerie Photo';
     firstModalDiv.appendChild(modalTitre);
 
-    const btnsContainer = document.createElement('div');
+     btnsContainer = document.createElement('div');
     btnsContainer.className = "btns-Modif";
     firstModalDiv.appendChild(btnsContainer);
 
-    const modGalleryContainer = document.createElement('div');
+     modGalleryContainer = document.createElement('div');
     modGalleryContainer.className = "mod-gallery";
     firstModalDiv.appendChild(modGalleryContainer);
 
@@ -86,7 +115,7 @@ function generateFirstModal() {
 
 
 
-    const deleteWorkBtn = document.createElement('a');
+     deleteWorkBtn = document.createElement('a');
 
     deleteWorkBtn.className = ('btn-delete-work');
     deleteWorkBtn.textContent = 'Supprimer les travaux';
@@ -115,13 +144,13 @@ function generateFirstModal() {
                 image.alt = work.title;
                 figure.appendChild(image);
 
-                const workId = work.id;
+                workId = work.id;
 
-                const trashIcon = document.createElement('i');
+                 trashIcon = document.createElement('i');
                 trashIcon.className = 'fas fa-trash-can';
                 figure.appendChild(trashIcon);
 
-                const figcaption = document.createElement('figcaption');
+                 figcaption = document.createElement('figcaption');
                 figcaption.textContent = work.title;
                 figure.appendChild(figcaption);
                 figure.setAttribute('data-workid', workId);
@@ -136,11 +165,11 @@ function generateFirstModal() {
             console.log('Une erreur s\'est produite lors de la récupération des works :', error);
         });
 
-    const figureElement = modGalleryContainer.querySelectorAll('.figure');
+  figureElement = modGalleryContainer.querySelectorAll('.figure');
     let currentWorkId = 1;
     figureElement.forEach(figure => {
         if (!figure.dataset.workid) {
-            const workId = String(currentWorkId);
+            workId = String(currentWorkId);
             figure.setAttribute('data-workid', workId);
             currentWorkId++;
         }
@@ -158,7 +187,7 @@ function generateFirstModal() {
     btnOpenSecondModal.addEventListener('click', generateSecondModal);
 
     if (!btnsContainer.querySelector('.btn-delete-work')) {
-        const deleteLInk = document.createElement('a');
+        deleteLInk = document.createElement('a');
         deleteLInk.href = '#';
         deleteLInk.textContent = 'supprimer la galerie';
         deleteLInk.className = 'btn-delete-work';
@@ -171,7 +200,7 @@ function generateFirstModal() {
 
 }
 
-const btnOpenFirstModal = document.querySelector('.a-modifier');
+ btnOpenFirstModal = document.querySelector('.a-modifier');
 btnOpenFirstModal.addEventListener('click', () => {
     mod.style.display = "flex";
     modal.style.display = "flex";
@@ -196,7 +225,7 @@ function generateSecondModal() {
     modal.appendChild(secondModalDiv);
 
 
-    const modalClose = document.createElement('a');
+    modalClose = document.createElement('a');
     modalClose.className = "modal_close";
     modalClose.textContent = "x";
     secondModalDiv.appendChild(modalClose);
@@ -209,13 +238,13 @@ function generateSecondModal() {
         // Redirigez l'utilisateur vers "index.html"
         window.location.href = "index.html";
     });
-    const secondeModalTitre = document.createElement('h3');
+     secondeModalTitre = document.createElement('h3');
     secondeModalTitre.className = 'modal_Tiltle';
     secondeModalTitre.textContent = "Ajouter une photo";
     secondModalDiv.appendChild(secondeModalTitre);
 
     // Créez les éléments pour la nouvelle modal et les éléments nécessaires
-    const uploadPhotoContainer = document.createElement('div');
+    uploadPhotoContainer = document.createElement('div');
     uploadPhotoContainer.className = "upload-photo-container";
 
 
@@ -224,17 +253,17 @@ function generateSecondModal() {
 
 
 
-    const inputChoiceContent = document.createElement('div');
+     inputChoiceContent = document.createElement('div');
     inputChoiceContent.className = 'inputs-seconde-modal-choices';
     uploadPhotoContainer.appendChild(inputChoiceContent);
 
-    const inputPhoto = document.createElement('input');
+     inputPhoto = document.createElement('input');
     inputPhoto.className = 'input-File-Seconde-Modal';
     inputPhoto.type = 'file';
     inputPhoto.accept = 'image/*';
     inputPhoto.id = 'fileInput';
 
-    const labelPhoto = document.createElement('label');
+     labelPhoto = document.createElement('label');
     labelPhoto.htmlFor = 'fileInput';
     labelPhoto.id = 'customFileInput'; // Vous pouvez également supprimer cet ID s'il n'est pas utilisé ailleurs
     labelPhoto.className = 'custom-file-input';
@@ -242,31 +271,31 @@ function generateSecondModal() {
 
     uploadPhotoContainer.appendChild(labelPhoto);
 
-    const btnValider = document.createElement('div');
+     btnValider = document.createElement('div');
     btnValider.className = "btn-valider";
     btnValider.textContent = "valider";
     btnValider.disable = true;
     secondModalDiv.appendChild(btnValider);
 
 
-    const inputTitle = document.createElement('input');
+    inputTitle   = document.createElement('input');
     inputTitle.className = 'input-new-title';
 
-    const labelTitre = document.createElement('label');
+     labelTitre = document.createElement('label');
     labelTitre.textContent = 'Titre';
     labelTitre.className = 'titre-input-titre';
 
-    const labelCategorie = document.createElement('label');
+     labelCategorie = document.createElement('label');
     labelCategorie.textContent = 'Catégorie';
 
-    const selectCategories = document.createElement('select');
+     selectCategories = document.createElement('select');
     selectCategories.className = 'input-categories-new-work';
 
-    const labelCategories = document.createElement('label');
+     labelCategories = document.createElement('label');
     labelCategories.textContent = 'Catégorie';
     labelCategories.className = 'Titre-select-categories'
 
-    const btnModal = document.createElement('input');
+     btnModal = document.createElement('input');
     btnModal.type = 'submit';
     btnModal.className = 'btn-add-photo';
     btnModal.textContent = 'Ajouter une photo';
@@ -289,7 +318,7 @@ function generateSecondModal() {
 
     btnValider.addEventListener('click', (event) => {
         event.preventDefault();
-        const file = inputPhoto.files[0]; // Récupérer le fichier sélectionné
+         file = inputPhoto.files[0]; // Récupérer le fichier sélectionné
         if (file) {
             ajouterTravailALaBaseDeDonnees(file);
             console.log("Nouveau travail ajouté !");
@@ -299,7 +328,7 @@ function generateSecondModal() {
 
 
     if (!btnModal) {
-        var btnsContainer = document.createElement('div');
+         btnsContainer = document.createElement('div');
         btnsContainer.className = 'btns-container';
         btnsContainer.appendChild(btnModal);
     }
@@ -310,7 +339,7 @@ function generateSecondModal() {
         .then(response => response.json())
         .then(data => {
             data.forEach(category => {
-                const option = document.createElement('option');
+                 option = document.createElement('option');
                 option.value = category.id;
                 option.textContent = category.name;
                 selectCategories.appendChild(option);
@@ -337,7 +366,7 @@ function generateSecondModal() {
             // Active le bouton d'ajout de travail s'il y a un fichier sélectionné
             btnValider.disabled = false;
             btnValider.classList.add('btn-actif');
-            const imagePreview = document.createElement('img');
+             imagePreview = document.createElement('img');
             imagePreview.src = URL.createObjectURL(file);
             imagePreview.className = 'image-preview';
 
@@ -351,7 +380,7 @@ function generateSecondModal() {
             // Désactive le bouton si aucun fichier n'est sélectionné
             btnValider.disabled = true;
 
-            const existingImagePreview = uploadPhotoContainer.querySelector('.image-preview');
+             existingImagePreview = uploadPhotoContainer.querySelector('.image-preview');
             if (existingImagePreview) {
                 uploadPhotoContainer.removeChild(existingImagePreview);
             }
@@ -362,7 +391,7 @@ function generateSecondModal() {
     
 
 
-    const modalCloseLink = document.querySelector('.modal_close');
+     modalCloseLink = document.querySelector('.modal_close');
 
     if (modalCloseLink) {
         console.log("Element '.modal_close' trouvé !");
